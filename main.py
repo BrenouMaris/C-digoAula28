468dbb1abf5963f796d3b48a9b9ab04c63547a54
BRENO ORTEGA MARIS <46862822845@info.local>
2026-03-16 21:34:10 -0300

    Código base

---
+++ main.py
@@ -0,0 +1,22 @@
+import processamento
+
+def main():
+    # Lista de tuplas conforme exigência do SENAI
+    alunos = [
+        ("Ana", [8.5, 9.0, 7.5]),
+        ("Bruno", [5.0, 6.0, 4.5]),
+        ("Carlos", []),
+        ("Diana", [10.0, 9.5, 9.8]),
+        ("Eduardo", [7.0, 8.0, 6.0]) 
+    ]
+
+    print("Processando dados...")
+    resultados, top_student = processamento.processar_alunos(alunos)
+    
+    print("Gerando arquivo .txt...")
+    processamento.gerar_relatorio(resultados, top_student)
+    
+    print("Sucesso! Verifique o arquivo 'resultado.txt'.")
+
+if __name__ == "__main__":
+    main()
